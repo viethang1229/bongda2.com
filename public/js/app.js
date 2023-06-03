703255228855343ee14e8ce7b8a1c5591ce71416
viethang1229 <viethang12292002@gmail.com>
2023-06-03 19:01:52 +0700

    update project football

---
+++ public/js/app.js
@@ -0,0 +1,13 @@
+
+window.onscroll = function() {myFunction()};
+
+var header = document.getElementById("scrol");
+var sticky = header.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
